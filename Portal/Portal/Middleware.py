c2984c940a41c37effb4ae3c1423e3c8a0f0b25d
Sriram Hariharan <76528489+Mythanos99@users.noreply.github.com>
2023-04-14 10:52:35 +0530

    Delete Portal directory

--- Portal/Portal/Middleware.py
+++
@@ -1,6 +0,0 @@
-from social_django.middleware import SocialAuthExceptionMiddleware
-
-class CustomSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
-    def get_message(self, request, exception):
-       default_msg = super(CustomSocialAuthExceptionMiddleware).get_message(request, exception) 
-       return "Sign In failed. Please Sign In manually."
--- Portal/Users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, RegisterView, profile
-
-urlpatterns = [
-    path('', home, name='users-home'),
-    path('register/', RegisterView.as_view(), name='users-register'),
-    path('profile/', profile, name='users-profile'),
-]
-
